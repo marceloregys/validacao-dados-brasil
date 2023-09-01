db900e94e2fcf6e4eaa9087863b935e38989cd24
Marcelo Regys Saraiva Gomes <marcelo.gomes@dock.tech>
2023-09-01 16:39:07 -0300

    Pegando dados do CEP utilizando Requests

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 #from cpf_cnpj import Documento
 #from TelefonesBr import TelefonesBr
-from datetime import datetime, timedelta
-from datas_br import DatasBr
+#from datas_br import DatasBr
+from acesso_cep import BuscaEndereco
 
-hoje = DatasBr()
-print(hoje.tempo_cadastro())
+
+# Pegar dados CEP
+cep = 31741377
+objeto_cep = BuscaEndereco(cep)
+
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+print(bairro, cidade, uf)
 
 
 
 '''
+# Datas
 cadastro = DatasBr()
 print(cadastro)
 
+hoje = DatasBr()
+print(hoje.tempo_cadastro())
+
+# Telefones
 telefone = "553187227409"
 telefone_objeto = TelefonesBr(telefone)
 print(telefone_objeto)
 
+# CPF/CNPJ
 exemplo_cnpj = "35379838000112"
 exemplo_cpf = "13473507644"
 documento = Documento.cria_documento(exemplo_cnpj)
