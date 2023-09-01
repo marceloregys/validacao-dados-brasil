5fdf6ef433729d125660814866184a0b55f9e16a
Marcelo Regys Saraiva Gomes <marcelo.gomes@dock.tech>
2023-09-01 13:02:08 -0300

    Validação Telefones - V1

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from cpf_cnpj import Documento
-from validate_docbr import CNPJ
+import re
+#from cpf_cnpj import Documento
+#from validate_docbr import CNPJ
+from TelefonesBr import TelefonesBr
 
-#cpf_um = Cpf("13473507644")
-#print(cpf_um)
+telefone = "553187227409"
 
+telefone_objeto = TelefonesBr(telefone)
+
+print(telefone_objeto)
+
+
+'''
 exemplo_cnpj = "35379838000112"
 exemplo_cpf = "13473507644"
 
@@ -11,3 +18,4 @@ documento = Documento.cria_documento(exemplo_cnpj)
 print(documento)
 documento = Documento.cria_documento(exemplo_cpf)
 print(documento)
+'''
